80d9567388f0d39835037dacc486bac68e857a05
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2024-01-10 17:33:27 +0000

    betty fx

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,24 +8,24 @@
 
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int aux = 0;
-    
-    if (array == NULL || size < 2)
+	size_t i, j;
+	int aux = 0;
+	
+	if (array == NULL || size < 2)
 		return;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        for (j = 0; j < size - i - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                aux = array[j];
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				aux = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = aux;
 				print_array(array, size);
-            }
-        }
+			}
+		}
 
-    } 
-} 
+	}
+}
