8691bf3d57a804cce3630a48139edc211ec59220
Gisezegk <124598948+Gisezegk@users.noreply.github.com>
2024-01-10 14:01:35 -0300

    .

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,12 +15,16 @@ void bubble_sort(int *array, size_t size)
 {
     int i, j;
     bool swapped;
+    
+    if (array == NULL || size < 2)
+		return;
+
     for (i = 0; i < n - 1; i++)
     {
         swapped = false;
         for (j = 0; j < n - i - 1; j++)
         {
-            if (arr[j] > arr[j + 1])
+            if (array[j] > array[j + 1])
             {
                 swap(&arr[j], &arr[j + 1]);
                 swapped = true;
