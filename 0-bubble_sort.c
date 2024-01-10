97e8e5ee7c8fcab043cc5039061b6ea09984b96d
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2024-01-10 17:10:11 +0000

    task 0 finished

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -5,32 +5,27 @@
  * @size: This is the size of the array.
 */
 
-void swap(int* x, int* y)
-{
-    int temp = *x;
-    *x = *y;
-    *y = temp;
-}
+
 void bubble_sort(int *array, size_t size)
 {
     size_t i, j;
-    bool swapped;
+    int aux = 0;
     
     if (array == NULL || size < 2)
 		return;
 
-    for (i = 0; i < n - 1; i++)
+    for (i = 0; i < size - 1; i++)
     {
-        swapped = false;
-        for (j = 0; j < n - i - 1; j++)
+        for (j = 0; j < size - i - 1; j++)
         {
             if (array[j] > array[j + 1])
             {
-                swap(&array[j], &array[j + 1]);
-                swapped = true;
+                aux = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = aux;
+				print_array(array, size);
             }
         }
-    if (swapped == false)
-        break;
+
     } 
 } 
