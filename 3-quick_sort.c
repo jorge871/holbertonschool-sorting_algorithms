3dbe70351c98300a0a2d8eaecfa2088a53e806b0
jorge871 <jg303114@gmail.com>
2024-01-11 20:27:50 -0300

    change the order of code in line 66

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -63,7 +63,7 @@ void quick_sort_recursive(int *array, int low, int high, size_t size_array)
 {
 	int in;
 
-	if (low >= high || low < 0)
+	if (high <= low || low < 0)
 		return;
 
 	in = pivot(array, low, high, size_array);
@@ -82,6 +82,5 @@ void quick_sort(int *array, size_t size)
 {
 	if (array == NULL)
 		return;
-
 	quick_sort_recursive(array, 0, size - 1, size);
 }
