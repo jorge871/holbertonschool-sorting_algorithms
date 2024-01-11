b4f0c08c31197fc3ce06af7c289dfa337fac0e84
jorge871 <jg303114@gmail.com>
2024-01-11 14:51:40 -0300

    fix the code

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,26 +8,31 @@
 
 void selection_sort(int *array, size_t size)
 {
-	size_t sel, i, j;
-	int aux = 0;
+	size_t  sel = 0, i = 0, j, aux = 0;
 
 	if (array == NULL)
 		return;
 
-	for (i = 0; i < size - 1; i++)
+	for (; i < (size - 1); i++)
 	{
 		sel = i;
-		for (j = i + 1; j < size; j++)
+		for (j = (i + 1); j < size; j++)
+		{
 			if (array[j] < array[sel])
+			{
 				sel = j;
-
-		if (sel != j)
+			}
+		}
+		if (sel != i)
 		{
 			aux = array[i];
 			array[i] = array[sel];
 			array[sel] = aux;
 			print_array(array, size);
 		}
-
+		else
+		{
+			continue;
+		}
 	}
 }
