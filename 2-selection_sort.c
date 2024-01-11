4ea061da8e318bf53135326ce17cba2a1ba94f32
jorge871 <jg303114@gmail.com>
2024-01-11 14:18:04 -0300

    add task 2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,9 +1,31 @@
 #include "sort.h"
 /**
- * selection_sort - Function that sorts an array of integers in ascending order.
+ * selection_sort - Function that sorts
+ * an array of integers in ascending order.
  * @array: This is the array this is the array to sort.
  * @size: This is the size of the array.
 */
 void selection_sort(int *array, size_t size)
 {
+		size_t sel, i, j, aux;
+
+	if (array == NULL)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		sel = i;
+		for (j = i + 1; j < size; j++)
+			if (array[j] < array[sel])
+				sel = j;
+
+		if (sel != j)
+		{
+			aux = array[i];
+			array[i] = array[sel];
+			array[sel] = aux;
+			print_array(array, size);
+		}
+
+	}
 }
