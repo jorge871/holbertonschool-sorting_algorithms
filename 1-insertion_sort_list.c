57020cc4cec8e80b97dcefabb1063fe5a0c9ad7c
Gisezegk <anatanonamewanamedesuka2@gmail.com>
2024-01-12 05:38:11 +0000

    insertion sort task

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,4 +7,37 @@
 */
 void insertion_sort_list(listint_t **list)
 {
+    listint_t *curr;
+
+    if (list == NULL || *list == NULL || (*list)->next == NULL)
+            return;
+
+    curr = (*list)->next;
+
+    while (curr != NULL)
+    {
+        listint_t *key = curr;
+        listint_t *prev = curr->prev;
+
+        while (prev != NULL && prev->n > key->n)
+        {
+            prev->next = key->next;
+            if (key->next != NULL)
+                    key->next->prev = prev;
+
+            key->prev = prev->prev;
+            key->next = prev;
+
+            if (prev->prev != NULL)
+                    prev->prev->next = key;
+            else
+                *list = key;
+
+            prev->prev = key;
+            prev = key->prev;
+
+            print_list(*list);
+        }
+        curr = curr->next;
+    }
 }
