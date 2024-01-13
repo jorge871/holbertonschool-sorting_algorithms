d24206654e6cf821a6b9b2a5c3dd54964debd96f
jorge871 <135288291+jorge871@users.noreply.github.com>
2024-01-12 23:03:23 -0300

    fix the error in betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,6 +1,6 @@
 #include "sort.h"
 /**
- * bubble sort - Function sorting an array of integers in ascending order
+ * bubble_sort - Function sorting an array of integers in ascending order
  * @array: This is the array this is the array to sort.
  * @size: This is the size of the array.
 */
